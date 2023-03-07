dda35f9f5ca28a9baf80f7409321e744361ad4f9
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-07 10:09:25 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import './App.css'
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 //This code is used to be a starting point for buildin a React application. 
 //Other components and fuctionality can be added to this base component to create complex applications.
 
 function App() { 
-  const title = 'Welcome to my portfolio!';
-
-   return (
+  return (
     <div className="App">
       <Navbar />
+      <h1>Welcome to my portfolio!</h1>
       <div className="content"></div>
         <Home />
+        <Header />
+        <Footer />      
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => { //Arrow fucntion
+    return ( 
+        
+        <footer className="footer">                    
+                    <h6>Copyright © 2022 Adriana Camarotto | All Rights Reserved.</h6>
+                    <div className="social-medias-icons">
+                        <ul>
+                            <li><a class="linkedin" href="https://www.linkedin.com/in/adriana-camarotto/" target="_blank"><i class="fa-brands fa-linkedin"></i>Linkedin</a></li>
+                            <li><a class="github" href="https://github.com/Adriana-Camarotto" target="_blank"><i class="fa-brands fa-github"></i>Github</a></li>
+                            <li><a class="instagram" href="https://www.instagram.com/adri.camarotto/" target="_blank"><i class="fa-brands fa-instagram"></i>Instagram</a></li>
+                            <li><a class="facebook" href="https://www.facebook.com/adri.camarotto" target="_blank"><i class="fa-brands fa-facebook"></i>Facebook</a></li>
+                        </ul>                                        
+                    </div>          
+        </footer>
+    );
+}
+ 
+export default Footer;
+
