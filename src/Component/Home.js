7cba6a2d7c24433509cd8f10bdf76f780dd12c42
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-05-30 12:12:36 +0100

    style update

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,40 +1,38 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
+import {  Col, Row } from "react-bootstrap";
 import myPhoto from "../Assets/AdriProfileImg.png";
 
 
 function Home() {
   return (
-      <section className="home">
-        <div className="container-fluid">
-          <div className="row">
-          <Col className="col-h1">          
-            <p>Hi there! I am</p>
-            <h1>ADRI CAMAROTTO</h1>
-            <h2>Front End Web Developer</h2>
-            
-            <hr className="my-4"></hr>
-            
-            <p>Check out my relevants projects</p>
-            <a
-              className="btn btn-outline-warning"
-              id="githuButton"
-              href="https://github.com/Adriana-Camarotto"
-              role="button"
-              target="_blank"
-              rel="noreferrer"
-            >
-              My Github
-            </a>         
-        </Col>
+        <section className="home container-fluid">
+          <Row className="row">
+            <Col className="col-h1">          
+              <p>Hi there! I am</p>
+              <h1>ADRI CAMAROTTO</h1>
+              <h2>Front End Web Developer</h2>
+              
+              <hr className="my-4"></hr>
+              
+              <p>Check out my relevants projects</p>
+              <a
+                className="btn btn-outline-warning"
+                id="githuButton"
+                href="https://github.com/Adriana-Camarotto"
+                role="button"
+                target="_blank"
+                rel="noreferrer"
+              >
+                My Github
+              </a>         
+            </Col>
 
-        <Col className="col-h2">
-          <img className="myPhoto" src={myPhoto} alt="Adriana Camarotto" />
-        </Col>
-          </div>
-        </div>
+            <Col className="col-h2">
+              <img className="myPhoto" src={myPhoto} alt="Adriana Camarotto" />
+            </Col>
+          </Row>        
       </section>
-     
+  
   );
 }
 
